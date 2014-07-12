c093a25ff35ef0c402d868895013bc6de84f8673
Jonathan Warner <jaxbot@gmail.com>
2014-07-12 19:51:03 -0400

    Import code from the old Android Studio project

--- src/me/sparklr/sparklrdroid/MainActivity.java
+++ src/me/sparklr/sparklrdroid/MainActivity.java
@@ -1,15 +1,128 @@
 package me.sparklr.sparklrdroid;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.app.Activity;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import java.io.*;
+import android.os.*;
 
-public class MainActivity extends Activity
-{
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity {
+    private WebView myWebView;
+
+    private ValueCallback<Uri> mUploadMessage;
+    private final static int FILECHOOSER_RESULTCODE=1;
+
+    Uri fileLocation;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(requestCode==FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage){
+                return;
+            }
+            if (resultCode != RESULT_OK) {
+                mUploadMessage.onReceiveValue(null);
+                mUploadMessage = null;
+                return;
+            }
+
+            Uri result = intent == null || resultCode != RESULT_OK ? null
+                    : intent.getData();
+            if (result == null) {
+                result = fileLocation;
+            }
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webView1);
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new MyWebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient()
+        {
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                mUploadMessage = uploadMsg;
+
+                Intent pickIntent = new Intent();
+                pickIntent.setType("image/*");
+                pickIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File f = new File(Environment.getExternalStorageDirectory(), "test.jpg");
+                fileLocation = Uri.fromFile(f);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                pickIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+
+                String pickTitle = "Select or take a photo";
+                Intent chooserIntent = Intent.createChooser(pickIntent, pickTitle);
+                chooserIntent.putExtra(
+                    Intent.EXTRA_INITIAL_INTENTS,
+                    new Intent[] { takePhotoIntent }
+                );
+                MainActivity.this.startActivityForResult(Intent.createChooser(chooserIntent, "Image Chooser"), FILECHOOSER_RESULTCODE);
+            }
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                openFileChooser(uploadMsg);
+            }
+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                openFileChooser(uploadMsg);
+            }
+        });
+        myWebView.loadUrl("http://sparklr.me/");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (myWebView.canGoBack()) {
+                        myWebView.goBack();
+                        return true;
+                    }
+                    break;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
 }
